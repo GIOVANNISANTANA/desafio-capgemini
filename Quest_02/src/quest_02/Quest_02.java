1ece6e5c9cd1d9bd7c2ac9f7a4baf3c67906c245
Giovanni Santana <49738899+GIOVANNISANTANA@users.noreply.github.com>
2022-03-06 11:55:53 -0300

    Ajuste de sintaxe

--- Quest_02/src/quest_02/Quest_02.java
+++ Quest_02/src/quest_02/Quest_02.java
@@ -11,8 +11,8 @@ public class Quest_02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);     // declarando a classe Scanner  
-        int ind, x, diferenca = 0;                  // declarando variaveis
+        Scanner input = new Scanner(System.in);                         // declarando a classe Scanner  
+        int ind, x, diferenca = 0;                                      // declarando variaveis
         
         System.out.println("Quantos números vão entrar no vetor? ");    //entrada de dados, tamanho do vetor e diferença procurada 
         ind = input.nextInt();                                         
@@ -20,12 +20,12 @@ public class Quest_02 {
         System.out.println("Qual valor da diferença procurada? ");
         x = input.nextInt();        
         
-        for (int a = 0; a < ind; a++) {            //loop para adicionar os valores ao vetor
+        for (int a = 0; a < ind; a++) {                                 //loop para adicionar os valores ao vetor
             System.out.println("Digite o valor de indice " + (a+1));
             n[a] = input.nextInt();
         }
         
-            for (int i = 0; i < n.length; i++){         //loop para organização do vetor em ordem decrescente
+            for (int i = 0; i < n.length; i++){                         //loop para organização do vetor em ordem decrescente
                 for (int j = 0; j < n.length; j++){
                     if (n[i] > n[j]){
                         int aux = n[i];
@@ -35,7 +35,7 @@ public class Quest_02 {
                 }
             }
         
-            for (int i = 0; i < n.length; i++){         //loop para testar as diferenças do vetor, caso enconte 1 unidade é adicionada a variavel diferenca
+            for (int i = 0; i < n.length; i++){                         //loop para testar as diferenças do vetor, caso enconte 1 unidade é adicionada a variavel diferenca
                 for (int j = 0; j < n.length; j++){
                     if ((n[i] - n[j]) == x){
                         diferenca++;
@@ -43,6 +43,6 @@ public class Quest_02 {
                 }         
             }
             
-            System.out.println("\n" + diferenca);       //impressao do valor diferenca pulando uma linha
+            System.out.println("\n" + diferenca);                       //impressao do valor diferenca pulando uma linha
     }
 }
